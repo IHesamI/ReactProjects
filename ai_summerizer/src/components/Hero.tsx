da9323b834a437a64f18fc6f63a759decd623044
hesam <bhesam2000@yahoo.com>
2023-09-04 13:04:45 +0430

    want to add store

--- ai_summerizer/src/components/Hero.tsx
+++ ai_summerizer/src/components/Hero.tsx
@@ -10,20 +10,28 @@ export default function Hero() {
         <img src={logo} className='w-28 object-contain' />
         <button
           type='button'
-          onClick={() => console.error('zarp')}
+          onClick={() =>
+            window.open('https://github.com/adrianhajdin')
+          }
           className='black_btn'
         >
           GitHub
         </button>
       </nav>
       <h1
-      className='head_text'>
+        className='head_text'>
         Summerize Articles With <br />
         <span
-        className='orange_gradient'>
+          className='orange_gradient'>
           OpenAi GPT-4
         </span>
       </h1>
+      <h2
+      className='desc'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Quos odit expedita mollitia, nobis enim tenetur? Aliquam earum laudantium
+        temporibus ea cumque quidem provident tempore optio. Voluptatum dicta repudiandae iusto temporibus.
+      </h2>
     </header>
   )
 }
---
+++ ai_summerizer/src/utility/useDelayInput.ts
@@ -0,0 +1,16 @@
+import { ChangeEventHandler, useEffect, useState } from 'react'
+
+export default function useDelayInput(delay: number = 1000) {
+    const [article, setArticle] = useState({
+        url: '',
+        summary: '',
+    })
+
+    let inputtimer = setTimeout(() => { }, delay)
+
+    const handelInput: ChangeEventHandler<HTMLInputElement> = (e: React.ChangeEvent<HTMLInputElement>) => {
+        clearTimeout(inputtimer)
+        inputtimer = setTimeout(() => setArticle({ ...article, url: e?.target?.value }), delay)
+    }
+    return {article, handelInput}
+}
